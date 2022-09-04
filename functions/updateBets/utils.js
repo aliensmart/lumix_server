f080b8683ef8ab4d2b828b33fe7aa59aa1d2b1f5
aliensmart <kaoua17@gmail.com>
2022-09-04 13:36:34 +0100

    added functionality when devs are created

--- functions/updateBets/utils.js
+++ functions/updateBets/utils.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const utils = require("../utils");
 const db = admin.firestore();
 // https://stackoverflow.com/questions/58577984/how-to-prevent-firebase-firestore-emulator-from-clearing-the-database-at-exit
 /**
@@ -25,7 +26,6 @@ const getAllPlayedUser = async (gameId) => {
     let userId = doc.ref.parent.parent.id;
     singleObj.amountPlayed = data.amountPlayed;
     singleObj.userId = userId;
-    console.log("userId ", userId);
     singleObj.numberInArray = Math.floor(data.amountPlayed / 500);
     maxEl = maxEl < singleObj.numberInArray ? singleObj.numberInArray : maxEl;
     for (let i = 0; i < singleObj.numberInArray; i++) {
@@ -36,65 +36,67 @@ const getAllPlayedUser = async (gameId) => {
   return { usersData: dataArr, players: playersIds, maxEl: maxEl };
 };
 
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-};
-
-const generateRandom = (min = 500, max = 1000) => {
-  // find diff
-  let difference = max - min;
-
-  // generate random number
-  let rand = Math.random();
-
-  // multiply with difference
-  rand = Math.floor(rand * difference);
-
-  // add with min value
-  rand = rand + min;
-
-  return rand;
-};
-// Returns a Promise that resolves after "ms" Milliseconds
-const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-const randomItem = (arr) => arr.splice((Math.random() * arr.length) | 0, 1);
 const chooseSingle = async (workedData, DocumentReference) => {
   const players = [...workedData?.players];
 
   const winNumber = workedData?.maxEl;
   await DocumentReference.update({ isShuffling: true });
 
-  for (let i = 0; i < 20; i++) {
-    shuffleArray(players);
-    let chosen = randomItem(players);
-    console.log(players);
-    const amount = 1000;
-    await db
-      .doc(`users/${chosen?.[0]}/userBets/${DocumentReference?.id}`)
-      .update({
-        won: admin.firestore.FieldValue.increment(amount),
-        isChosen: true,
-        lastUpdated: admin.firestore.Timestamp.now(),
-      })
-      .catch((e) => console.log(e));
-    await db
-      .doc(`users/${chosen?.[0]}/`)
-      .update({
-        totalWon: admin.firestore.FieldValue.increment(amount),
-      })
-      .catch((e) => console.log(e));
-  }
+  const winningAmount = [0, 0, 0, 500, 750, 1000, 1500];
 
+  for (let i = 0; i < winNumber; i++) {
+    const randomAmount = utils.randomizer(winningAmount);
+    utils.shuffleArray(players);
+    let chosen = utils.randomItem(players);
+
+    const amount = winningAmount[randomAmount];
+    if (amount > 0) {
+      await db
+        .doc(`users/${chosen?.[0]}/userBets/${DocumentReference?.id}`)
+        .update({
+          won: admin.firestore.FieldValue.increment(amount),
+          isChosen: true,
+          lastUpdated: admin.firestore.Timestamp.now(),
+        })
+        .catch((e) => console.log(e));
+      await db
+        .doc(`users/${chosen?.[0]}/`)
+        .update({
+          totalWon: admin.firestore.FieldValue.increment(amount),
+        })
+        .catch((e) => console.log(e));
+    }
+  }
+  // console.log(randomAmount);
   await DocumentReference.update({ status: "ENDED", isShuffling: false });
+  // await
 };
 
 module.exports.runGame = async (DocumentReference) => {
   const workedData = await getAllPlayedUser(DocumentReference.id);
-  console.log(workedData);
-  chooseSingle(workedData, DocumentReference);
+  // console.log(workedData);
+
+  // update all document of devs when game start playing
+  await Promise.all(
+    workedData?.usersData?.map((userdata) => {
+      db.doc(
+        `users/${userdata?.userId}/userBets/${DocumentReference.id}`
+      ).update({ status: "PLAYING" });
+    })
+  ).catch((e) => console.log(e));
+  // Choose winners
+  await chooseSingle(workedData, DocumentReference).catch((e) =>
+    console.log(e)
+  );
+
+  // update all document of devs after choosing docs
+  await Promise.all(
+    workedData?.usersData?.map((userdata) => {
+      db.doc(
+        `users/${userdata?.userId}/userBets/${DocumentReference.id}`
+      ).update({ status: "ENDED" });
+    })
+  ).catch((e) => console.log(e));
+
+  // u
 };
--- functions/utils.js
+++ functions/utils.js
@@ -11,10 +11,76 @@ module.exports.recursiveCollectionDelete = async (documentReference) => {
     // Otherwise, delete the document.
     for (let doc of querySnapshot.docs) {
       // Recurse for each document.
-      await this.recursiveDeleteSubcollections(doc.ref);
+      await this.recursiveCollectionDelete(doc.ref);
     }
   }
 
   // Delete the document if no subcollections exist.
   await documentReference.delete();
+};
+
+/**
+ * Takes an array of items then return a random index
+ * @example
+ * ```
+ * const arr = [1, 2, 4, 5]
+ * const randomIndex = randomizer(arr)
+ * const randomPick = arr[randomIndex]
+ * ```
+ * @param {array} arr
+ * @returns index
+ */
+module.exports.randomizer = (arr) => Math.floor(Math.random() * arr.length);
+
+/**
+ * This is a shufller, which suffle an array of items them makes the array unpredictable
+ * @example
+ * ```
+ * const arr = [1, 2, 3, 4]
+ * shuffleArray(arr)
+ * console.log(arr) => [1,3,4,2]
+ * ```
+ * @param {array} array
+ */
+module.exports.shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+// Returns a Promise that resolves after "ms" Milliseconds
+module.exports.timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+/**
+ * This takes an array of items then randomly pick and item then remove the picked item from the array
+ * * @example
+ * ```
+ * const arr = [1, 2, 3, 4]
+ * const randomPick  = randomItem(arr)
+ * console.log(randomPick) => [2]
+ * console.log(arr) => [1, 3, 4]
+ * ```
+ * @param {array} arr
+ * @returns array of single item
+ */
+module.exports.randomItem = (arr) =>
+  arr.splice((Math.random() * arr.length) | 0, 1);
+
+const generateRandom = (min = 500, max = 1000) => {
+  // find diff
+  let difference = max - min;
+
+  // generate random number
+  let rand = Math.random();
+
+  // multiply with difference
+  rand = Math.floor(rand * difference);
+
+  // add with min value
+  rand = rand + min;
+
+  return rand;
 };
