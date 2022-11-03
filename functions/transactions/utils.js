831445a9ab319aa0c0b42141e96ec1034ca4b4fe
aliensmart <kaoua17@gmail.com>
2022-11-03 04:48:46 -0400

    working on header section

--- functions/transactions/utils.js
+++ functions/transactions/utils.js
@@ -1,7 +1,23 @@
+const axios = require("axios");
 // create exportable module for the function onTransactionCreate
 module.exports.onTransactionCreate = async (userData, transactionData) => {
   // TODO
   // Login to cinetpay
+  let logindata = {
+    apikey: "303961111635796d9a226b6.09891449",
+    password: "RHuzZJ7arHEaJ@w",
+  };
+  var config = {
+    method: "post",
+    url: "https://client.cinetpay.com/v1/auth/login",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Access-Control-Allow-Origin": "*",
+    },
+    data: JSON.stringify(logindata),
+  };
+  const resp = await axios(config).catch((e) => console.log("error ", e));
+  console.log(resp);
   // create contact with user email, phone, name, surname, prefix
   // update user doc with isAddedContact true
 };
